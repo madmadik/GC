b89b272faff20a50b2677060d0650f9c0736556c
КурмангалиевМ <@A-305-08>
2017-12-27 12:15:07 +0600

    Initial commit

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            int generation = GC.GetGeneration(test);
+            Console.WriteLine("поколение = " + generation);
+
+            GC.Collect(generation);
+            int generation2 = GC.GetGeneration(test);
+            Console.WriteLine("поколение = " + generation2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp23/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Test
+    { 
+        public String Name { get; set; }
+    }
+}
